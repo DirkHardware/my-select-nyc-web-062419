0fee564725c11ba668c047fbbff6927d2c91bd71
Anderson Rolf <email.the.anderson@gmail.com>
2019-06-19 15:59:00 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,12 +1,11 @@
 def my_select(array)
 counter = 0
 result = []
-
 while counter < array.size
   if yield(array[counter]) == true
   result.push(array[counter])
 end
-  counter+=1
+  counter += 1
 end
 result
 end
